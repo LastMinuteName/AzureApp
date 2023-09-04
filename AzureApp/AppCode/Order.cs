2b9c95540d7ab3f7c651738d2c76f5e0e5cfa288
LastMinuteName <LastMinuteName@hotmail.com>
2023-09-04 23:26:54 +1000

    Add project files.

---
+++ AzureApp/AppCode/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureApp.AppCode {
+    public class Order {
+        public float average { get; set; }
+        public string? date { get; set; }
+        public float highest { get; set; }
+        public float lowest { get; set; }
+        public int order_count { get; set; }
+        public int volume { get; set; }
+
+    }
+}
---
+++ AzureApp/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const fetchData = async () => {
+    let fetchedData = await fetch('https://esi.evetech.net/latest/markets/10000002/history/?datasource=tranquility&type_id=44992')
+    fetchedData = await fetchedData.json();
+    console.log(fetchedData);
+}
+
+fetchData();
